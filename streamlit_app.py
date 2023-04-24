7f84f002b838e30cbb4ab148de0010aa2123fb7a
liamgdt00 <122754086+liamgdt00@users.noreply.github.com>
2023-04-24 16:17:52 +0100

    Update streamlit_app.py
    typo fix

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,5 +23,5 @@ fruit_choice = streamlit.text_input('What fruit would you like information about
 streamlit.write('The user entered', fruit_choice)
 fruityvice_response = requests.get('https://fruityvice.com/api/fruit/' + fruit_choice)
 
-fruitvice_normalized = pd.json_normalize(fruityvice_response.json())
+fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
 streamlit.dataframe(fruityvice_normalized)
